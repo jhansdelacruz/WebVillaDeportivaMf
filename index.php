038e6e1ea4369e0f2e173cde6e929f872af98805
Jhans de la Cruz <jhansdlcd@gmail.com>
2022-07-25 16:52:52 -0500

    Version v.0.0.1

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="title" content="Villa Deportiva | Monsefú" />
+  <meta http-equiv="content-language" content="es" />
+  <meta name="description" content="Te invitamos a la más cómoda y moderna Villa deportiva, Ubicanos en Monsefú..." />
+  <meta name="MobileOptimized" content="width" />
+  <meta name="HandheldFriendly" content="true" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <title>Villa Deportiva || Monsefú</title>
+  <link rel="stylesheet" href="css/normalize.css" />
+  <link rel="stylesheet" href="css/global.css" />
+  <link rel="stylesheet" href="css/header.css" />
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="icon" href="img/public/logo.png" />
+  <link rel="apple-touch-ico" href="img/public/logo.png" />
+  <link rel="theme-color" content="#2f6627" />
+</head>
+
+<body>
+  <?php
+  require('./layout/header.php');
+  ?>
+
+  <main role="main">
+    <article role="article" class="modefull">
+
+
+      <div class="container">
+        <div class="contet">
+          <!--imagen de la sede-->
+          <div class="host-picture">
+            <article class="field-host-picture">
+              <div class="name-field-host-picture">
+                <img src="./img/home/VD-Monsefu.png" alt="Villa Deportiva Monsefú" title="Villa Deportiva Monsefú" typeof="foaf:Image">
+              </div>
+            </article>
+          </div>
+          <div class="host-content-wrap">
+            <marquee>
+              <div class="center-monsefu">
+                <a href="#" hreflang="es">Visítanos</a>
+              </div>
+            </marquee>
+
+            <div class="content-title">
+              <h1 class="title">
+                <span class="name-title">Villa Deportiva - Monsefú</span>
+              </h1>
+            </div>
+            <div class="content-adress">
+              <div class="host-adress">
+                <div class="field-label">Dirección</div>
+                <div class="field-item">LOTE A PREDIO SAN ANTONIO SECTOR EL PALMO</div>
+              </div>
+            </div>
+            <div class="spectators-number">
+              <div class="optime">
+                <div class="field-labell">AREA TOTAL</div>
+
+                <div class="field-item">4500 mts cuadrados</div>
+              </div>
+
+            </div>
+            <div class="opening_time">
+              <div class="optime">
+                <div class="field-labell">E-MAIL</div>
+
+                <div class="field-item">contratos@villadeportiva.com</div>
+              </div>
+            </div>
+            <div class="btnmode">
+              <div class="host-tickets">
+                <a class="link-tickets" href="https://tickets.lima2019.pe/showEventCriteria.html?changeLanguageTo=es" target="_blank">
+                  <span> Ver Galeria
+                  </span>
+                </a>
+              </div>
+              <div class="host-map">
+                <a class="link-en-mapa" href="https://goo.gl/maps/KpmZiYqZW5BrMbSn6" target="_blank"> <span> Ver Mapa
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </article>
+    <article class="artPrim">
+      <section class="divPrim">
+        <h2>hola</h2>
+      </section>
+    </article>
+
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+  </main>
+  <footer></footer>
+  <script src="js/app.js"></script>
+</body>
+
+</html>
---
+++ js/app.js
@@ -0,0 +1,43 @@
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.hcontainer");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 50) {
+    header.style.backgroundColor = "#F8F9FA";
+    header.style.boxShadow = "0px 8px 10px #bebebe";
+  } else {
+    header.style.backgroundColor = "#FFFFFF";
+    header.style.boxShadow = "0px 1px 10px #bebebe";
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
+});
+
+let ubicacionHeader = window.pageYOffset;
+window.onscroll = function () {
+  let desplazamientoActual = window.pageYOffset;
+  if (ubicacionHeader >= desplazamientoActual) {
+    document.getElementById("headerPosition").style.top = "0";
+  } else {
+    document.getElementById("headerPosition").style.top = "-100px";
+    document.getElementById("headerPosition").style.transition = "0.7s";
+  }
+  ubicacionHeader = desplazamientoActual;
+};
