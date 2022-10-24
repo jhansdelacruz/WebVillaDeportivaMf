1ef6dfe30b4ba02d66ac87eb3e06a0e5bceac36c
Jhans de la Cruz <jhansdlcd@gmail.com>
2022-10-24 13:03:58 -0500

    añadiendo js a los details

--- js/details.js
+++ js/details.js
@@ -1,2 +1,32 @@
-const details = document.querySelectorAll("details");
-console.log(details);
+// const details = document.getElementById("detailPreguntas");
+// const deta = document.querySelectorAll("details");
+
+// console.log(details);
+
+// details.addEventListener("click", (e) => {
+//   if (
+//     e.target.tagName === "SUMMARY" ||
+//     e.target.tagName === "SPAN" ||
+//     e.target.tagName === "DETAILS"
+//   ) {
+//     console.log(e, "sii");
+//     e.target.classList.add("detailOpen");
+//   }
+// });
+function cambioPanel(index) {
+  let node = document.querySelectorAll("details")[index];
+  let nodeOpen = document.querySelectorAll("details[open]")[0];
+
+  if (node.hasAttribute("open")) {
+    //Cerramos el panel actual
+    node.classList.remove("detailOpen");
+  } else {
+    //Cerramos el ultimo panel abierto si lo hay
+    if (nodeOpen) {
+      nodeOpen.removeAttribute("open");
+      nodeOpen.classList.remove("detailOpen");
+    }
+    //Abrimos el panel actual
+    node.classList.add("detailOpen");
+  }
+}
